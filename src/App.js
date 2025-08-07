993378274693750c4806a2623730c537457357f7
Gabriele Re <gabriele.re@studenti.fauser.edu>
2025-08-07 15:55:22 +0200

    Deploying website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Button, Flex, Spinner } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hi, this is the king blog</p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Amoreeee</p>
+        <Spinner></Spinner>
+        <Flex minWidth="max-content" alignItems="center" gap="2">
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            onClick={() => {
+              console.log("clicked main button");
+            }}
+          >
+            Ti
+          </Button>
+          <Button colorScheme="teal" variant="outline">
+            Amo
+          </Button>
+        </Flex>
       </header>
     </div>
   );
